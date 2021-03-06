e5f5ceb595647843641b939c2a5b779a811afda4
Samuel Lima <samuellima810@gmail.com>
2021-03-06 16:47:14 -0300

    finalizando a questao 12

--- 02-estrutura de decisão/EstruturaDecisao.py
+++ 02-estrutura de decisão/EstruturaDecisao.py
@@ -238,6 +238,180 @@ if (preco3 > preco1 and preco1 > preco2):
     ordemDecrescente.append(preco2)
     print("Ordem Decrescente: ", ordemDecrescente)
 
+# =============================================================================
+# 10 - Faça um Programa que pergunte em que turno você estuda. 
+#    Peça para digitar M-matutino ou V-Vespertino ou N- Noturno. 
+#   Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+# =============================================================================
+
+print("Digite o turno que vc estuda ")
+turnoDeEstudo = input("M - Matutino\n V - Vespertino\n N - Noturno\n")
+
+turnoDeEstudo = turnoDeEstudo.lower()
+
+if turnoDeEstudo == 'm':
+    print("Bom dia !")
+elif turnoDeEstudo == 'v':
+    print("Boa tarde !")
+elif turnoDeEstudo == 'n':
+    print("Boa noite !")
+else:
+    print("Valor invalido !")
+    
+    
+# =============================================================================
+# 11 - As Organizações Tabajara resolveram dar um aumento de salário aos seus 
+# colaboradores e lhe contraram para desenvolver o programa que calculará os reajustes.
+#       
+#        Faça um programa que recebe o salário de um colaborador e o reajuste segundo 
+#        o seguinte critério, baseado no salário atual:
+#    a) salários até R$ 280,00 (incluindo) : aumento de 20%
+#    b) salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
+#    c) salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
+#    d) salários de R$ 1500,00 em diante : aumento de 5% Após o aumento ser realizado, informe na tela:
+    #    1) o salário antes do reajuste;
+    #    2) o percentual de aumento aplicado;
+    #    3) o valor do aumento;
+    #    4) o novo salário, após o aumento.
+# =============================================================================
+
+salarioDoFuncionario = float(input("Digite o salario: "))
+
+if salarioDoFuncionario <= 280.00:
+    print("Salario antes do reajuste = R$ ", salarioDoFuncionario)
+    print("Percentual aplicado 20% = R$ ", salarioDoFuncionario*0.20)
+    print("Novo Salario apos aumento = R$ ", salarioDoFuncionario*1.20)
+elif salarioDoFuncionario > 280.00 and salarioDoFuncionario <= 700.00:
+    print("Salario antes do reajuste = R$ ", salarioDoFuncionario)
+    print("Percentual aplicado 15% = R$ ", salarioDoFuncionario*0.15)
+    print("Novo Salario apos aumento = R$ ", salarioDoFuncionario*1.15)
+elif salarioDoFuncionario > 700.00 and salarioDoFuncionario < 1500:
+    print("Salario antes do reajuste = R$ ", salarioDoFuncionario)
+    print("Percentual aplicado 15% = R$ ", salarioDoFuncionario*0.10)
+    print("Novo Salario apos aumento = R$ ", salarioDoFuncionario*1.10)
+elif salarioDoFuncionario >= 1500.00:
+    print("Salario antes do reajuste = R$ ", salarioDoFuncionario)
+    print("Percentual aplicado 5% = R$ ", salarioDoFuncionario*0.05)
+    print("Novo Salario apos aumento = R$ ", salarioDoFuncionario*1.05)
+
+# =============================================================================
+# 12 - QUESTÃO
+# =============================================================================
+"""
+Faça um programa para o cálculo de uma folha de pagamento, sabendo que os descontos são do Imposto de Renda, 
+que depende do salário bruto (conforme tabela abaixo) e 3% para o Sindicato e que o FGTS corresponde a 11% 
+do Salário Bruto, mas não é descontado (é a empresa que deposita). O Salário Líquido corresponde ao Salário 
+Bruto menos os descontos. O programa deverá pedir ao usuário o valor da sua hora e a quantidade de horas 
+trabalhadas no mês.
+
+Desconto do IR:
+    
+Salário Bruto até 900 (inclusive) - isento
+Salário Bruto até 1500 (inclusive) - desconto de 5%
+Salário Bruto até 2500 (inclusive) - desconto de 10%
+Salário Bruto acima de 2500 - desconto de 20% Imprima na tela as informações, dispostas conforme o exemplo abaixo. 
+No exemplo o valor da hora é 5 e a quantidade de hora é 220.
+
+        Salário Bruto: (5 * 220)        : R$ 1100,00
+        (-) IR (5%)                     : R$   55,00  
+        (-) INSS ( 10%)                 : R$  110,00
+        FGTS (11%)                      : R$  121,00
+        Total de descontos              : R$  165,00
+        Salário Liquido                 : R$  935,00
+
+"""
+
+valorPorHora = float(input("digite o valor q vc ganha por HORA: "))
+numeroHorasTrabalhadasPorMes = float(input("digite o numero de horas trabalhadas por mes: "))
+
+#considerando o mês com 30 dias
+#cosiderando o dia 24h e o mês 720h
+salarioNoMes = numeroHorasTrabalhadasPorMes*valorPorHora
+
+print("Salario no mês: ", salarioNoMes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
